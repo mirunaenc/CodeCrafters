119c65c729774bc8555117b7059c632b4f9e25fe
CondreaIustin <iustin.condrea@student.unitbv.ro>
2023-11-11 09:25:11 -0800

    Added class Bulldozer

---
+++ Project Modern C++/Project Modern C++/bulldozer.h
@@ -0,0 +1,29 @@
+#pragma once
+import board;
+import pylon;
+
+
+namespace twixt {
+
+    class Bulldozer {
+    private:
+        size_t m_linePosition;
+        size_t m_columnPosition;
+        Board& m_board;
+
+    public:
+ 
+        Bulldozer(Board& board, size_t initialLinePosition, size_t initialColumnPosition);
+        Bulldozer(const Bulldozer& other);  
+        Bulldozer(Bulldozer&& other) noexcept; 
+        Bulldozer& operator=(const Bulldozer& other); 
+        Bulldozer& operator=(Bulldozer&& other) noexcept;  
+        ~Bulldozer();
+
+        size_t getLinePosition() const;
+        size_t getColumnPosition() const;
+        void setLinePosition(size_t linePosition);
+        void setColumnPosition(size_t columnPosition);
+    };
+
+}
