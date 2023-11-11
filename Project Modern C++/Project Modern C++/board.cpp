98c7bf0b1bfef54c24d17f2486ca15027cd0a029
2andreas8 <andreas.faur@student.unitbv.ro>
2023-11-11 13:40:54 +0200

    Added std::optional to m_board in Board class.

--- Project Modern C++/Project Modern C++/board.cpp
+++ Project Modern C++/Project Modern C++/board.cpp
@@ -4,26 +4,24 @@ import <iostream>;
 import <algorithm>;
 import <iterator>;
 import <vector>;
+import <optional>;
 import pylon;
 
 twixt::Board::Board(uint16_t size) : m_size{ size } {
-	m_board.resize(m_size);
+    m_board = std::vector<Pylon>(size * size);
 }
 
 twixt::Board& twixt::Board::operator=(const Board& board)
 {
     if (this != &board) {
         m_size = board.m_size;
-        m_board.resize(m_size);
-        std::copy(board.m_board.begin(), board.m_board.end(), back_inserter(m_board));
+        m_board = board.m_board;
     }
     return *this;
 }
 
-twixt::Board::Board(const Board& board) : m_size{ board.m_size } {
-        m_board.resize(m_size);
-        std::copy(board.m_board.begin(), board.m_board.end(), back_inserter(m_board));
-}
+twixt::Board::Board(const Board& board) : m_size{ board.m_size }, m_board{ board.m_board } 
+{}
 
 uint16_t twixt::Board::getSize() const {
     return m_size;
@@ -31,12 +29,12 @@ uint16_t twixt::Board::getSize() const {
 
 void twixt::Board::setSize(uint16_t size) {
     m_size = size;
-    m_board.resize(m_size);
+    m_board = std::vector<Pylon>(size * size);
 }
 
 void twixt::Board::setPylon(uint16_t line, uint16_t column, const Pylon& pylon) {
     if (line < m_size && column < m_size) {
-        m_board[line * m_size + column] = pylon;
+        m_board.value()[line * m_size + column] = pylon;
     }
     else {
         std::cout << "Error at setting a pylon.";
@@ -46,19 +44,20 @@ void twixt::Board::setPylon(uint16_t line, uint16_t column, const Pylon& pylon) 
 
 const twixt::Pylon& twixt::Board::getPylon(uint16_t line, uint16_t column) const {
     if (line < m_size && column < m_size) {
-        return m_board[line * m_size + column];
+        return m_board.value()[line * m_size + column];
     }
     else {
         std::cout << "Error at method getPylon.";
-        exit(0);
+        return Pylon{};
     }
 }
 
 void twixt::Board::resetPosition(uint16_t line, uint16_t column) {
     if (line < m_size && column < m_size) {
-        m_board[line * m_size + column] = null;
+        m_board.value()[line * m_size + column] = Pylon{};
     }
 }
+
 
 
 
--- Project Modern C++/Project Modern C++/board.cppm
+++ Project Modern C++/Project Modern C++/board.cppm
@@ -2,31 +2,24 @@
 
 export module board;
 import <array>;
-import pylon;
 import <optional>;
 import <vector>;
+import pylon;
 
 namespace twixt {
 	export class Board {
 
 	private:
 		uint16_t m_size;
-		std::vector<Pylon> m_board;
+		std::optional<std::vector<Pylon>> m_board;
 	public:
 		Board(uint16_t size);
-
-		Board& operator=(const Board& board);
-
 		Board(const Board& board);
-
+		Board& operator=(const Board& board);
 		uint16_t getSize() const;
-
 		void setSize(uint16_t size);
-
 		void setPylon(uint16_t line, uint16_t column, const Pylon& pylon);
-
 		const Pylon& getPylon(uint16_t line, uint16_t column) const;
-
 		void resetPosition(uint16_t line, uint16_t column);
 	};
 }
