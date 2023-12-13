1ca98ef5c8346fda572fddaf34d39332292dce5e
mirunaenc <mirunastefania88@gmail.com>
2023-12-13 10:08:05 +0200

    Implemented RecommenderSystem class in dll files.

--- Project Modern C++/RecommenderSystem/Recommender/RecommenderSystem.cpp
+++ Project Modern C++/RecommenderSystem/Recommender/RecommenderSystem.cpp
@@ -1,1 +1,22 @@
 #include "RecommenderSystem.h"
+
+recommender::RecommenderSystem::RecommenderSystem()
+{
+}
+
+recommender::RecommenderSystem::~RecommenderSystem()
+{
+}
+
+std::pair<int, int> recommender::RecommenderSystem::getMoveRecommendation()
+{
+	return std::pair<int, int>();
+}
+
+void recommender::RecommenderSystem::requestHint()
+{
+}
+
+void recommender::RecommenderSystem::displayRecommendation(const std::pair<int, int>& move)
+{
+}
--- Project Modern C++/RecommenderSystem/Recommender/RecommenderSystem.h
+++ Project Modern C++/RecommenderSystem/Recommender/RecommenderSystem.h
@@ -3,3 +3,17 @@
 #else
 #define RECOMMENDERSYSTEM_API __declspec(dllimport)
 #endif
+#include <utility>
+
+namespace recommender {
+    class RECOMMENDERSYSTEM_API RecommenderSystem {
+    public:
+        RecommenderSystem();
+        ~RecommenderSystem();
+        std::pair<int, int> getMoveRecommendation(/* parametrii necesari */);
+        void requestHint();
+        void displayRecommendation(const std::pair<int, int>& move);
+    private:
+
+    };
+}
