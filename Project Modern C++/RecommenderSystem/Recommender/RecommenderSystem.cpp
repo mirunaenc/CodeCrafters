6d1bbb32a3ee29bed9f51a47d29769e2b6457d9e
mirunaenc <mirunastefania88@gmail.com>
2023-12-13 10:14:38 +0200

    Implemented requestHint().

--- Project Modern C++/RecommenderSystem/Recommender/RecommenderSystem.cpp
+++ Project Modern C++/RecommenderSystem/Recommender/RecommenderSystem.cpp
@@ -1,4 +1,5 @@
 #include "RecommenderSystem.h"
+#include <iostream>
 
 recommender::RecommenderSystem::RecommenderSystem()
 {
@@ -15,6 +16,14 @@ std::pair<int, int> recommender::RecommenderSystem::getMoveRecommendation()
 
 void recommender::RecommenderSystem::requestHint()
 {
+    std::cout << "Ganditi-va la urmatoarea mutare sau apasati 'h' pentru un hint." << std::endl;
+    char userInput;
+    std::cin >> userInput;
+    if (userInput == 'h' || userInput == 'H') {
+        std::pair<int, int> hint = getMoveRecommendation(/* parametrii necesari */);
+        std::cout << "Hint: Plasati un pilon la linia " << hint.first
+            << " si coloana " << hint.second << "." << std::endl;
+    }
 }
 
 void recommender::RecommenderSystem::displayRecommendation(const std::pair<int, int>& move)
