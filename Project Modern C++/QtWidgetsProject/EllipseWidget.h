9505aca78e90ffbec15d96274c72c592a481ad01
CondreaIustin <iustin.condrea@student.unitbv.ro>
2024-01-13 05:30:20 -0800

    Added EllipseWidget

---
+++ Project Modern C++/QtWidgetsProject/EllipseWidget.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <QApplication>
+#include <QWidget>
+#include <QGridLayout>
+#include <QLabel>
+#include <QPalette>
+#include <QPainter>
+
+class EllipseWidget : public QWidget
+{
+    Q_OBJECT
+signals:
+    void clicked();
+private:
+    QColor color;
+public:
+    explicit EllipseWidget(QWidget* parent = nullptr) : QWidget(parent), color(Qt::white) {}
+  
+};
